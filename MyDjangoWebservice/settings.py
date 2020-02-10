c2ce977ae9b3e3e13fc7b01cda9ee1f6b1245620
SIGESI <45672753+SIGESI@users.noreply.github.com>
2020-02-10 21:00:48 +0100

    Update settings.py
    chang setting ALLOWED_HOSTS

--- MyDjangoWebservice/settings.py
+++ MyDjangoWebservice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7e@2vlwgb34)&=1%wai332fxvi51$lj6va^^tqe&tm2s3p=(&i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
